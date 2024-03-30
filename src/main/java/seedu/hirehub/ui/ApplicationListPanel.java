0bb40bdeff95002aa7ddc3ef68f566591d836c30
0liverkong <oliverkong03@gmail.com>
2024-03-30 18:26:35 +0800

    Add column to display applications

---
+++ src/main/java/seedu/hirehub/ui/ApplicationListPanel.java
@@ -0,0 +1,45 @@
+package seedu.hirehub.ui;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.hirehub.commons.core.LogsCenter;
+import seedu.hirehub.model.application.Application;
+
+import java.util.logging.Logger;
+
+public class ApplicationListPanel extends UiPart<Region> {
+    private static final String FXML = "ApplicationListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(ApplicationListPanel.class);
+
+    @javafx.fxml.FXML
+    private ListView<Application> applicationListView;
+
+    /**
+     * Creates a {@code ApplicationListPanel} with the given {@code ObservableList}.
+     */
+    public ApplicationListPanel(ObservableList<Application> applicationList) {
+        super(FXML);
+        applicationListView.setItems(applicationList);
+        applicationListView.setCellFactory(listView -> new ApplicationListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     */
+    class ApplicationListViewCell extends ListCell<Application> {
+        @Override
+        protected void updateItem(Application application, boolean empty) {
+            super.updateItem(application, empty);
+
+            if (empty || application == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new ApplicationCard(application, getIndex() + 1).getRoot());
+            }
+        }
+    }
+}
